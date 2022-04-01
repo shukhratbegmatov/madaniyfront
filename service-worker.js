f6a5e613b0ec24cbf01e4b582ecc80f8ef5bf69f
Пользователь <shuukhratbegmatov@gmail.com>
2022-04-01 11:03:34 +0500

    madaniy statistic countup

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82c474df282532a04e3c20d82ed8ad9b.js"
+  "/precache-manifest.4dc4adf87d69c8061f52985d5c3540ac.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "madaniymerosv2"});
